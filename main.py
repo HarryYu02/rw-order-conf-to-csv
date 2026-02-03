7ee72b6a219520c21e77b5cf9728b9428e65428e
Harry Y. <harryyu02@gmail.com>
2026-02-03 17:42:11 -0500

    bulk convert

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import os
 from pypdf import PdfReader
 
 from parser import (
@@ -8,33 +9,67 @@ from parser import (
 )
 
 def main():
-    if len(sys.argv) < 2:
-        raise Exception("Error: order confirmation not provided")
+    # usage: uv run main.py <input> <output_dir>
+    if len(sys.argv) < 3:
+        raise Exception("Error: invalid arguments")
         sys.exit(1)
-    order_conf = sys.argv[1]
-    if not order_conf.endswith(".pdf"):
-        raise Exception("Error: order confirmation not a pdf")
+    input = sys.argv[1]
+    files_to_parse = []
+    if os.path.isfile(input):
+        files_to_parse.append(input)
+    elif os.path.isdir(input):
+        contents = list(map(lambda item: os.path.join(input, item), os.listdir(input)))
+        files_to_parse += contents
+    else:
+        raise Exception("Error: invalid input")
         sys.exit(1)
 
-    reader = PdfReader(order_conf)
-    data = []
-    if len(reader.pages) == 0:
-        raise Exception("Error: order confirmation have no pages")
+    output_dir = sys.argv[2]
+    if not os.path.isdir(output_dir):
+        raise Exception("Error: invalid output directory")
         sys.exit(1)
-    first_page = reader.pages[0]
-    meta_data = extract_meta_data(first_page.extract_text())
-    for page in reader.pages:
-        raw_text = page.extract_text()
-        parsed = parse_page(raw_text)
-        data += parsed
 
-    output_location = f"csv/{order_conf[4:-4]}.csv"
-    headers = ["Style", "Size", "Quantity", "Status", "Net Price", "Amount"]
-    formatted_data = list(map(lambda e: [e.style_id, e.size, e.quantity, e.status, e.net_price, e.total_price], data))
-    with open(output_location, "x", newline="", encoding="utf-8-sig") as f:
-        writer = csv.writer(f)
-        writer.writerow(headers)
-        writer.writerows(formatted_data)
+    for file in files_to_parse:
+        filename = os.path.basename(file)
+        name_part, extension = os.path.splitext(filename)
+        if extension != ".pdf":
+            print(f"skipping {file}...")
+            continue
+
+        print(f"converting {file}...")
+        reader = PdfReader(file)
+        data = []
+        if len(reader.pages) == 0:
+            raise Exception("Error: order confirmation have no pages")
+            sys.exit(1)
+        first_page = reader.pages[0]
+        meta_data = extract_meta_data(first_page.extract_text())
+        for page in reader.pages:
+            raw_text = page.extract_text()
+            parsed = parse_page(raw_text)
+            data += parsed
+
+        if len(data) == 0:
+            raise Exception("Error: no entry found in given pdf")
+
+        output_location = os.path.join(output_dir, f"{name_part}.csv")
+        headers = ["Order #", "Order date", "PO #", "Style", "Size", "Quantity", "Status", "Net Price", "Amount"]
+        formatted_data = list(map(lambda e: [
+            meta_data.order_num,
+            meta_data.order_date,
+            meta_data.po_num,
+            e.style_id,
+            e.size,
+            e.quantity,
+            e.status,
+            e.net_price,
+            e.total_price
+        ], data))
+        # TODO: check if file exist
+        with open(output_location, "x", newline="", encoding="utf-8-sig") as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
+            writer.writerows(formatted_data)
 
 
 if __name__ == "__main__":
